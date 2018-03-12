c91c5f1669a56eb476fc93ac732e88f4b23a664a
Holger Nahrstaedt <holger@nahrstaedt.de>
2018-03-12 12:28:13 +0100

    revert changes

--- steembase/bip38.py
+++ steembase/bip38.py
@@ -43,11 +43,13 @@ def _encrypt_xor(a, b, aes):
 
 def encrypt(privkey, passphrase):
     """ BIP0038 non-ec-multiply encryption. Returns BIP0038 encrypted privkey.
+
     :param privkey: Private key
     :type privkey: Base58
     :param str passphrase: UTF-8 encoded passphrase for encryption
     :return: BIP0038 non-ec-multiply encrypted wif key
     :rtype: Base58
+
     """
     privkeyhex = repr(privkey)  # hex
     addr = format(privkey.uncompressed.address, "BTC")
@@ -74,12 +76,14 @@ def encrypt(privkey, passphrase):
 
 def decrypt(encrypted_privkey, passphrase):
     """BIP0038 non-ec-multiply decryption. Returns WIF privkey.
+
     :param Base58 encrypted_privkey: Private key
     :param str passphrase: UTF-8 encoded passphrase for decryption
     :return: BIP0038 non-ec-multiply decrypted key
     :rtype: Base58
     :raises SaltException: if checksum verification failed (e.g. wrong
     password)
+
     """
 
     d = unhexlify(base58decode(encrypted_privkey))
